1100a124269ab3bf0e117314a2d7f153e088455c
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-05-03 12:13:27 +0900

    Solve problem 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+	if (Math.sign(num) == -1) {
+		return "ERROR";
+	}
+	return string.repeat(num);
 };
 
 // Do not edit below this line
