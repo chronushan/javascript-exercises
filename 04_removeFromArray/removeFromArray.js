6f4b3b6244f79297b855ab6c8c24fae5dbcbc2d2
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-05-03 14:58:16 +0900

    Test 1 pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, remove) {
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] == remove) {
+			arr.splice(i, 1);
+			console.log(arr);
+			break;
+		}
+	}
 
+	return arr;
 };
 
 // Do not edit below this line
