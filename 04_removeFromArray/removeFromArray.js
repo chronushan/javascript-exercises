4ca7c129eebd2c7f84cddc05f33553c0684991db
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-05-03 18:14:51 +0900

    Pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,15 @@
-const removeFromArray = function (arr, remove1, remove2) {
+const removeFromArray = function (arr, remove1, remove2, remove3, remove4) {
 	let result = [];
 	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] != remove1 && arr[i] != remove2) {
+		if (
+			arr[i] !== remove1 &&
+			arr[i] !== remove2 &&
+			arr[i] !== remove3 &&
+			arr[i] !== remove4
+		) {
 			result.push(arr[i]);
 		}
+		// if else (remove1)
 	}
 
 	return result;
