de6b6ec6955e521d4fe300cf22f4722a0b715c34
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-05-05 16:32:54 +0900

    Pass all tests with flying colors

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,14 @@
 const leapYears = function (year) {
 	if (year % 100 == 0) {
-		return false;
+		if (year % 400 == 0) {
+			return true;
+		} else {
+			return false;
+		}
 	} else if (year % 4 == 0) {
 		return true;
+	} else {
+		return false;
 	}
 };
 
