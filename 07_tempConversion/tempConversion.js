614153829601cdc911ec86a9093ed3f670c44adc
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-05-05 18:53:24 +0900

    Pass the rest of the test with flying colors

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,13 @@
 const convertToCelsius = function (num) {
-	let result = (num - 32) * (5 / 9);
+	let result = Math.round((num - 32) * (5 / 9) * 10) / 10;
+	// let result = (num - 32) * (5 / 9);
+	// return Number(result.toFixed(1));
+	// return Math.round(result * 10) / 10;
 	return result;
 };
 
 const convertToFahrenheit = function (num) {
-	let result = (num * 9) / 5 + 32;
+	let result = Math.round(((num * 9) / 5 + 32) * 10) / 10;
 	return result;
 };
 
