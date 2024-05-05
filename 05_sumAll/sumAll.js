f1025d8a94c99fe3cf16282f3d11e4f279c6b6f9
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-05-05 13:54:17 +0900

    Pass test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+	let answer = 0;
+	for (let i = start; i <= end; i++) {
+		answer += i;
+	}
+	return answer;
 };
 
 // Do not edit below this line
