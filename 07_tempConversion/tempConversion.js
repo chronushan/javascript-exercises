5a60e996cde075451e6dd728ac8f799a837622b8
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-05-05 18:39:36 +0900

    Pass test 1

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+	let result = (num - 32) * (5 / 9);
+	return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+	let result = (num * 9) / 5 + 32;
+	return result;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
