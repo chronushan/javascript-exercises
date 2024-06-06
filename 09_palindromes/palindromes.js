fb2eb2a41f8f71b2af545b4ff22ee711368db262
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-06-06 17:37:19 +0900

    Solve test 1 and 2

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
+const palindromes = function (a) {
+	let reversedStr = "";
+	let stringWithoutPunc = "";
+	for (let i = 0; i < a.length; i++) {
+		if ((a[i] >= "a" && a[i] <= "z") || (a[i] >= "A" && a[i] <= "Z")) {
+			stringWithoutPunc += a[i];
+		} else {
+			continue;
+		}
+		// console.log(stringWithoutPunc);
+	}
+	for (let i = a.length - 1; i >= 0; i--) {
+		if ((a[i] >= "a" && a[i] <= "z") || (a[i] >= "A" && a[i] <= "Z")) {
+			reversedStr += a[i];
+		} else {
+			continue;
+		}
+		// console.log(reversedStr);
+	}
 
+	if (stringWithoutPunc == reversedStr) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// palindromes("racecar!");
