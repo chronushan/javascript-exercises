d7742eec41b94c7c36c653e2b810ad961c7ecb67
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-06-06 16:57:48 +0900

    Solve the rest of problem #8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -12,11 +12,29 @@ const sum = function (array) {
 	return answer;
 };
 
-const multiply = function () {};
+const multiply = function (array) {
+	let answer = array[0];
+	array.forEach((a) => {
+		if (a == array[0]) {
+			return;
+		} else {
+			answer *= a;
+		}
+	});
+	return answer;
+};
 
-const power = function () {};
+const power = function (a, b) {
+	return Math.pow(a, b);
+};
 
-const factorial = function () {};
+const factorial = function (a) {
+	let answer = 1;
+	for (let i = 1; i <= a; i++) {
+		answer *= i;
+	}
+	return answer;
+};
 
 // Do not edit below this line
 module.exports = {
