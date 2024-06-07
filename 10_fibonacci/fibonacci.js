ab3e8dec356d8d94d19961ae2833c1f6db767bb5
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-06-07 11:24:08 +0900

    Solve prolem 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+	let firstPrev = 1;
+	let secondPrev = 0;
+	for (let i = 2; i < n; i++) {
+		let current = firstPrev + secondPrev;
+		firstPrev = current;
+		secondPrev = firstPrev;
+	}
+	return firstPrev;
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (a) {
+	return a.reduce((accumulator, currentArray) => {
+		accumulator.push(currentArray.title);
+		return accumulator;
+	}, []);
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+// const books = [
+// 	{
+// 		title: "Book",
+// 		author: "Name",
+// 	},
+// 	{
+// 		title: "Book2",
+// 		author: "Name2",
+// 	},
+// ];
+
+// getTheTitles(books);
