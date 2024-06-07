79e2a56debe7970a3d61db4637a3d5fc92830af7
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-06-07 11:47:14 +0900

    Solve problem 12 test 1

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (a) {
+	let biggest = { yearOfBirth: 0, yearOfDeath: 0 };
+	a.forEach(function (a) {
+		if (
+			biggest.yearOfDeath - biggest.yearOfBirth <
+			a.yearOfDeath - a.yearOfBirth
+		) {
+			biggest = a;
+		}
+	});
+	return biggest;
 };
 
 // Do not edit below this line
