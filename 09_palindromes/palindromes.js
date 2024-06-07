cb6ba34d03e53f8f7bdc3d1ed470a1b724c89c8e
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-06-07 09:42:30 +0900

    Solve the rest of the test cases

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,7 +2,11 @@ const palindromes = function (a) {
 	let reversedStr = "";
 	let stringWithoutPunc = "";
 	for (let i = 0; i < a.length; i++) {
-		if ((a[i] >= "a" && a[i] <= "z") || (a[i] >= "A" && a[i] <= "Z")) {
+		if (
+			(a[i] >= "a" && a[i] <= "z") ||
+			(a[i] >= "A" && a[i] <= "Z") ||
+			(a[i] >= "0" && a[i] <= "9")
+		) {
 			stringWithoutPunc += a[i];
 		} else {
 			continue;
@@ -10,7 +14,11 @@ const palindromes = function (a) {
 		// console.log(stringWithoutPunc);
 	}
 	for (let i = a.length - 1; i >= 0; i--) {
-		if ((a[i] >= "a" && a[i] <= "z") || (a[i] >= "A" && a[i] <= "Z")) {
+		if (
+			(a[i] >= "a" && a[i] <= "z") ||
+			(a[i] >= "A" && a[i] <= "Z") ||
+			(a[i] >= "0" && a[i] <= "9")
+		) {
 			reversedStr += a[i];
 		} else {
 			continue;
@@ -19,6 +27,10 @@ const palindromes = function (a) {
 	}
 	stringWithoutPunc = stringWithoutPunc.toLowerCase();
 	reversedStr = reversedStr.toLowerCase();
+
+	// console.log(reversedStr);
+	// console.log(stringWithoutPunc);
+
 	if (stringWithoutPunc == reversedStr) {
 		return true;
 	} else {
