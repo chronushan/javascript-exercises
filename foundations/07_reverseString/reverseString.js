0e185119399ecaaf2f966cfd6bdc8f3421fb2cad
Henry Han <48421873+chronushan@users.noreply.github.com>
2026-04-08 16:36:40 +0900

    Solve problem 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+	let answer = [];
+	for (let i = 0; i < string.length; i++) {
+		answer.push(string[i]);
+	}
+	return answer.reverse().join("");
 };
 
 // Do not edit below this line
