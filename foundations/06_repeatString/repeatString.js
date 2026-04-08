3ff1fcf8b16ecf081c0c7fba8e6f407e6781d110
Henry Han <48421873+chronushan@users.noreply.github.com>
2026-04-08 16:29:08 +0900

    Solve problem 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+	let answer = [];
+	if (num < 0) {
+		return "ERROR";
+	}
+	for (let i = 0; i < num; i++) {
+		answer.push(string);
+	}
+	return answer.join("");
 };
 
 // Do not edit below this line
