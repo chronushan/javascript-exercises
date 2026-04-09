c7b2723faee879a563542a8b02a31e78e79ff1c2
Henry Han <48421873+chronushan@users.noreply.github.com>
2026-04-09 15:11:11 +0900

    Solve problem 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+	let answer = 0;
+	let temp = 0;
+	if (
+		a < 0 ||
+		b < 0 ||
+		typeof a != "number" ||
+		typeof b != "number" ||
+		Math.floor(a) != a ||
+		Math.floor(b) != b
+	)
+		return "ERROR";
+	if (b < a) {
+		temp = b;
+		b = a;
+		a = temp;
+	}
+	for (a; a <= b; a++) {
+		answer += a;
+	}
+	return answer;
 };
 
 // Do not edit below this line
