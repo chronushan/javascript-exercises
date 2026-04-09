448b46c975343d190be7c830b627bdb096ad3014
Henry Han <48421873+chronushan@users.noreply.github.com>
2026-04-09 14:50:15 +0900

    Solve problem 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...words) {
+	let i = 0;
+	let answer = array.filter((item) => !words.includes(item));
+
+	return answer;
 };
 
 // Do not edit below this line
