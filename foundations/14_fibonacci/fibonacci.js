0de678128de920b70294927b150b661dbf2e78be
Henry Han <48421873+chronushan@users.noreply.github.com>
2026-04-20 19:09:37 +0900

    Solve 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+	let a = 1;
+	let b = 0;
+	for (let i = 2; i <= n; i++) {
+		let result = a + b;
+		b = a;
+		a = result;
+	}
+	return a;
 };
 
+console.log(fibonacci(6));
 // Do not edit below this line
 module.exports = fibonacci;
