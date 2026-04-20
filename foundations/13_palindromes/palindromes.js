5d41b2be2428118719e37636df7006419e3b6585
Henry Han <48421873+chronushan@users.noreply.github.com>
2026-04-20 18:24:52 +0900

    Solve problem 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,32 @@
-const palindromes = function () {
+const palindromes = function (str = "RAcecar") {
+	let lowerStr = str.toLowerCase();
+	let array = lowerStr.split("");
+	let strippedStr = [];
 
+	for (let i = 0; i < str.length; i++) {
+		if (
+			(lowerStr.charCodeAt(i) < 65 && lowerStr.charCodeAt(i) > 57) ||
+			lowerStr.charCodeAt(i) < 48 ||
+			lowerStr.charCodeAt(i) > 132
+		) {
+			console.log(lowerStr[i]);
+			continue;
+		}
+		strippedStr.push(array[i]);
+	}
+
+	strippedStr = strippedStr.join("");
+	console.log(strippedStr);
+
+	let arrayRev = [];
+	for (let i = strippedStr.length - 1; i >= 0; i--) {
+		arrayRev.push(strippedStr[i]);
+	}
+	arrayRev = arrayRev.join("");
+	console.log(arrayRev);
+	return strippedStr == arrayRev ? true : false;
 };
 
+console.log(palindromes("A car, a man, a maraca."));
 // Do not edit below this line
 module.exports = palindromes;
---
+++ foundations/13_palindromes/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+let arrayRev = [];
+	// for (let i = array.length - 1; i >= 0; i--) {
+	// 	if (array.charAt(-1) > 172 || array.charAt(-1) < 41) {
+	// 		continue;
+	// 	}
+	// 	arrayRev.push(array[i]);
+	// }
+	// arrayRev = arrayRev.join("");
+
+	// return lowerStr == arrayRev ? true : false;
